775826f150269b0ddf3cfa93e1e9fa19316b6256
Juhyun Suh <shipalnomoo@gmail.com>
2022-08-03 12:44:00 -0500

    Deployment ready

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
@@ -9,29 +10,34 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, '/../dist')));
 app.use(express.json());
 
+// Initializing.
+let questionId;
+let answerId;
+let photoId;
+(async () => {
+  try {
+    questionId = await db.Question.countDocuments({});
+    answerId = await db.CombinedAnswer.countDocuments({});
+    photoId = await db.CombinedAnswer.aggregate([
+      { $unwind: '$photos' },
+      {
+        $group: {
+          _id: null,
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    console.log(`q: ${questionId}, a: ${answerId}, p:${photoId[0].count}`);
+  } catch (err) {
+    console.error(err);
+  }
+  console.log('Finished Initializing');
+})();
+
 // Find an answer
 app.get('/qa/:id/answers', (req, res) => {
   db.findCombinedAnswer({ question_id: req.params.id })
     .then((data) => res.send(data))
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-});
-
-// Takes a very long time!
-app.get('/qa/:id/slowAnswers', (req, res) => {
-  Promise.all([
-    db.findAnswer({ question_id: req.params.id }),
-    db.findAnswerPhoto({ answer_id: req.params.id }),
-  ])
-    .then(([data, photos]) => {
-      res.send({
-        // eslint-disable-next-line no-underscore-dangle
-        ...data._doc,
-        photos,
-      });
-    })
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
@@ -57,7 +63,7 @@ app.post('/qa/:id', (req, res) => {
     body: data.body,
     date_written: Date.now(),
     helpful: 0,
-    id: 0,
+    id: questionId++,
     product_id: req.params.id,
     reported: 0,
   })
@@ -71,35 +77,90 @@ app.post('/qa/:id', (req, res) => {
 // Submit an answer
 app.post('/qa/:id/answers', (req, res) => {
   const data = req.body;
-  const promises = [];
-  if (data.photos.length !== 0) {
-    data.photos.forEach((photo) => {
-      promises.push(
-        db.createAnswerPhoto({
-          answer_id: req.params.id,
-          url: photo,
-        })
-      );
+  db.createCombinedAnswer({
+    asker_email: data.email,
+    asker_name: data.name,
+    body: data.body,
+    date_written: Date.now(),
+    helpful: 0,
+    id: answerId++,
+    question_id: req.params.id,
+    reported: 0,
+    photos: data.photos,
+  })
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
     });
-  }
-  Promise.all(promises)
-    .then(() => {
-      db.createAnswer({
-        asker_email: data.email,
-        asker_name: data.name,
-        body: data.body,
-        date_written: Date.now(),
-        helpful: 0,
-        id: 0,
-        question_id: req.params.id,
-        reported: 0,
-      })
-        .then(() => res.sendStatus(201))
-        .catch((err) => {
-          console.error(err);
-          res.sendStatus(500);
-        });
-    })
+});
+
+// markQAsHelpful
+app.put('/qa/question/:qid/helpful', (req, res) => {
+  db.Question.findOneAndUpdate(
+    {
+      id: req.params.qid,
+    },
+    {
+      $inc: { helpful: 1 },
+    }
+  )
+    .exec()
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+// reportQuestion
+app.put('/qa/question/:qid/report', (req, res) => {
+  db.Question.findOneAndUpdate(
+    {
+      id: req.params.qid,
+    },
+    {
+      $inc: { reported: 1 },
+    }
+  )
+    .exec()
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+// markAnsAsHelpful
+app.put('/qa/answer/:aid/helpful', (req, res) => {
+  db.CombinedAnswer.findOneAndUpdate(
+    {
+      id: req.params.aid,
+    },
+    {
+      $inc: { helpful: 1 },
+    }
+  )
+    .exec()
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+// reportAns
+app.put('/qa/answer/:aid/report', (req, res) => {
+  db.CombinedAnswer.findOneAndUpdate(
+    {
+      id: req.params.aid,
+    },
+    {
+      $inc: { reported: 1 },
+    }
+  )
+    .exec()
+    .then(() => res.sendStatus(201))
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
